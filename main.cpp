a439965313d55d014c71cd3e984ef1ba5783d6ef
zxkredo <lukasmaga3@gmail.com>
2024-01-06 15:54:00 +0100

    Different pattern to test

--- main.cpp
+++ main.cpp
@@ -6,19 +6,20 @@ void startCalculator(GameState& gamestate)
     gamestate.start_checking_cells();
 }
 int main() {
-    std::vector<std::vector<bool>> vektor = std::vector(800, std::vector(350, false));
+    std::vector<std::vector<bool>> vektor = std::vector(5, std::vector(5, false));
     vektor.at(1).at(2) = true;
-    vektor.at(1).at(3) = true;
+    vektor.at(2).at(1) = true;
     vektor.at(2).at(2) = true;
     vektor.at(2).at(3) = true;
+    vektor.at(3).at(2) = true;
 
-    GameState gamestate(800,350);
+    GameState gamestate(5,5);
     gamestate.insert_current_world(vektor);
 
     std::vector<std::thread*> threads;
 
     threads.reserve(1);
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 1; ++i) {
         threads.emplace_back(new std::thread(startCalculator, std::ref(gamestate)));
     }
 
