8c47afcc5f7a496b0e452238e35832c654c0451b
zxkredo <lukasmaga3@gmail.com>
2024-01-08 14:56:00 +0100

    ServerSaver class for saving patterns on server, added saveWorldToServer

---
+++ headers/ServerSaver.h
@@ -0,0 +1,25 @@
+//
+// Created by Tatry on 8. 1. 2024.
+//
+
+#ifndef POSGAMEOFLIFECLIENT_SERVERSAVER_H
+#define POSGAMEOFLIFECLIENT_SERVERSAVER_H
+
+
+#include <vector>
+#include <string>
+
+class ServerSaver {
+    typedef std::vector<std::vector<bool>> world_t;
+private:
+    static const char trueCellChar = 'X';
+    static const char falseCellChar = '.';
+    static const char endRowChar = '\n';
+public:
+    static void saveWorldToServer(world_t worldToSave);
+    static void getWorldFromServer(std::string fileName);
+
+};
+
+
+#endif //POSGAMEOFLIFECLIENT_SERVERSAVER_H
---
+++ source files/ServerSaver.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Tatry on 8. 1. 2024.
+//
+
+#include <sstream>
+#include "../headers/ServerSaver.h"
+#include "../headers/my_socket.h"
+
+void ServerSaver::saveWorldToServer(world_t worldToSave) {
+    MySocket* socket = MySocket::createConnection("frios2.uniza.sk", 18235);
+    std::stringstream dataToSend;
+    for (const auto &row : worldToSave) {
+
+        for (const bool &cell: row)
+        {
+            if (cell)
+            {
+                dataToSend << ServerSaver::trueCellChar;
+            }
+            else
+            {
+                dataToSend << ServerSaver::falseCellChar;
+            }
+            dataToSend << ServerSaver::endRowChar;
+        }
+    }
+    socket->sendData(dataToSend.str());
+}
+
+void ServerSaver::getWorldFromServer(std::string fileName) {
+}
